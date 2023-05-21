86c36dc0382ab381ace6bf7078df67762f02df27
George Romanov <gosha.egorian@gmail.com>
2023-05-21 23:45:47 +0800

    Fix prettier and pull request action

--- src/routes/edit-form/+page.svelte
+++ src/routes/edit-form/+page.svelte
@@ -23,7 +23,7 @@
     childrenTicketPriceStore,
     currencyStore,
     submitForm
-  } from "./stores";
+  } from './stores';
 </script>
 
 <main>
@@ -32,57 +32,57 @@
 
     <label class="input_block">
       <span>Name:</span>
-      <input type="text" on:input={changeName} value="{$nameStore}">
+      <input type="text" on:input={changeName} value={$nameStore} />
     </label>
 
     <label class="input_block">
       <span>Link:</span>
-      <input type="text" on:input={changeLink} value="{$linkStore}">
+      <input type="text" on:input={changeLink} value={$linkStore} />
     </label>
 
     <label class="input_block">
       <span>Start date:</span>
-      <input type="date" on:input={changeStart} value="{$startStore}">
+      <input type="date" on:input={changeStart} value={$startStore} />
     </label>
 
     <label class="input_block">
       <span>Remind date:</span>
-      <input type="date" on:input={changeRemind} value="{$remindStore}">
+      <input type="date" on:input={changeRemind} value={$remindStore} />
     </label>
 
     <label class="input_block">
       <span>Max adults amount:</span>
-      <input type="number" on:input={changeMaxAdults} value="{$maxAdultsStore}">
+      <input type="number" on:input={changeMaxAdults} value={$maxAdultsStore} />
     </label>
 
     <label class="input_block">
       <span>Max children amount:</span>
-      <input type="number" on:input={changeMaxChildren} value="{$maxChildrenStore}">
+      <input type="number" on:input={changeMaxChildren} value={$maxChildrenStore} />
     </label>
 
     <label class="input_block">
       <span>Max adults per reservation:</span>
-      <input type="number" on:input={changeMaxAdultsPerReservation} value="{$maxAdultsPerReservationStore}">
+      <input type="number" on:input={changeMaxAdultsPerReservation} value={$maxAdultsPerReservationStore} />
     </label>
 
     <label class="input_block">
       <span>Max children per reservation:</span>
-      <input type="number" on:input={changeMaxChildrenPerReservation} value="{$maxChildrenPerReservationStore}">
+      <input type="number" on:input={changeMaxChildrenPerReservation} value={$maxChildrenPerReservationStore} />
     </label>
 
     <label class="input_block">
       <span>Adult ticket price:</span>
-      <input type="number" on:input={changeAdultTicketPrice} value="{$adultTicketPriceStore}">
+      <input type="number" on:input={changeAdultTicketPrice} value={$adultTicketPriceStore} />
     </label>
 
     <label class="input_block">
       <span>Children ticket price:</span>
-      <input type="number" on:input={changeChildrenTicketPrice} value="{$childrenTicketPriceStore}">
+      <input type="number" on:input={changeChildrenTicketPrice} value={$childrenTicketPriceStore} />
     </label>
 
     <label class="input_block">
       <span>Currency:</span>
-      <input type="text" on:input={changeCurrency} value="{$currencyStore}">
+      <input type="text" on:input={changeCurrency} value={$currencyStore} />
     </label>
 
     <button type="submit">Create event</button>
