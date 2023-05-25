e8689f0432b7fb556c241f0c3263197d74f0e127
George Romanov <gosha.egorian@gmail.com>
2023-05-25 16:08:22 +0800

    fix: Add user info (just for test)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+  import { userFirstNameStore, userIDStore, userLastNameStore } from "./logic";
+</script>
+
+<p>Hello, {$userLastNameStore} {$userFirstNameStore}! Your id is {$userIDStore}</p>
+
 <a href="./edit-form">Create new event</a>
