b7bd7d9cb69aa44bf6c53720b1d5dae11ab471be
George Romanov <gosha.egorian@gmail.com>
2023-05-25 16:25:12 +0800

    fix: Add telegram script on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { userFirstNameStore, userIDStore, userLastNameStore } from "./logic";
+  import TelegramScript from "$lib/TelegramScript.svelte";
 </script>
+
+<TelegramScript />
 
 <p>Hello, {$userLastNameStore} {$userFirstNameStore}! Your id is {$userIDStore}</p>
 
