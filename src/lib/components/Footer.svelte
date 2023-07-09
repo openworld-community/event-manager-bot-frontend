cf9a7b596a1f94cb1e47717e1883d14235265365
kosmos-954 <tired.d.albert@gmail.com>
2023-07-09 13:39:18 +0600

    Added version to footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  let version = 'Unknown';
+
+  onMount(async () => {
+    const res = await fetch('/version.txt');
+    version = await res.text();
+  });
+</script>
+
+<footer>
+  Frontend Version: {version}
+</footer>
