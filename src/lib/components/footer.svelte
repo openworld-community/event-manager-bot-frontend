1c58b53768f1573ba41bba5828656a399b777069
hellokkki <akibigbabytape@gmail.com>
2023-09-19 19:18:05 +0300

    upd clean and position

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -5,27 +5,46 @@
    
   function toggle() {
     togglePopup(true)
-    console.log('toggled')
   }
 </script>
 
 <footer class="footer">
-  <Button
-  size='60px'
-  height='30px'
-  on:click = {() => toggle()}
-  >
-    Меню
-  </Button>
+  <div class="footer--menu-btn-box">
+    <Button
+    size='60px'
+    height='30px'
+    borderRadius='12px'
+    on:click = {() => toggle()}
+    >
+    <div class="footer--menu-button">
+      <p>X</p>
+      <p>Меню</p>
+    </div>
+    </Button>
+  </div>
   <p>hello im footer</p>
+
 </footer>
 
-<style>
+<style lang="scss">
   .footer {
     width: 340px;
     height: 48px;
     display: grid;
     grid-template-columns: 80px 1fr 2px;
 
+    &--menu-btn-box {
+      margin-top: 14px;
+    }
+
+    &--menu-button {
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: center;
+      gap: 5px;
+
+      height: inherit;
+    }
   }
 </style>
