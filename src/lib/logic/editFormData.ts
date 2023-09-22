b3a38cf5ec652590b39a15b2c6f41d7f8f7b7bfb
hellokkki <akibigbabytape@gmail.com>
2023-09-22 17:25:38 +0300

    add variants to inputData

--- src/lib/logic/editFormData.ts
+++ src/lib/logic/editFormData.ts
@@ -5,6 +5,7 @@ type InputData = {
   systemName: string;
   initValue: string;
   type: HTMLInputTypeAttribute;
+  variant: string
 };
 
 export const editFormData: InputData[] = [
@@ -12,67 +13,78 @@ export const editFormData: InputData[] = [
     label: 'Name',
     systemName: 'name',
     initValue: '',
-    type: 'text'
+    type: 'text',
+    variant: 'form-event'
   },
 
   {
     label: 'Link',
     systemName: 'link',
     initValue: '',
-    type: 'text'
+    type: 'text',
+    variant: 'form-event'
   },
   {
     label: 'Start date',
     systemName: 'start',
     initValue: '',
-    type: 'date'
+    type: 'date',
+    variant: 'form-event'
   },
   {
     label: 'Remind date',
     systemName: 'remind',
     initValue: '',
-    type: 'date'
+    type: 'date',
+    variant: 'form-event'
   },
   {
     label: 'Max adults amount',
     systemName: 'max_adults',
     initValue: '100',
-    type: 'number'
+    type: 'number',
+    variant: 'form-event'
   },
   {
     label: 'Max children amount',
     systemName: 'max_children',
     initValue: '100',
-    type: 'number'
+    type: 'number',
+    variant: 'form-event'
   },
   {
     label: 'Max adults per reservation',
     systemName: 'max_adults_per_reservation',
     initValue: '4',
-    type: 'number'
+    type: 'number',
+    variant: 'form-event'
   },
   {
     label: 'Max children per reservation',
     systemName: 'max_children_per_reservation',
     initValue: '4',
-    type: 'number'
+    type: 'number',
+    variant: 'form-event'
   },
   {
     label: 'Adult ticket price',
     systemName: 'adult_ticket_price',
     initValue: '100',
-    type: 'number'
+    type: 'number',
+    variant: 'form-event'
   },
   {
     label: 'Children ticket price',
     systemName: 'children_ticket_price',
     initValue: '50',
-    type: 'number'
+    type: 'number',
+    variant: 'form-event'
   },
   {
     label: 'Currency',
     systemName: 'currency',
     initValue: 'USD',
-    type: 'text'
+    type: 'text',
+    variant: 'form-event'
   }
 ];
