a4c774aa9a99183569647a317a52936771074ac2
hellokkki <akibigbabytape@gmail.com>
2023-09-28 17:12:02 +0300

    add tooltip menu arrow icon

---
+++ src/lib/components/icons/MenuArrowUp.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let size = 20;
+    export let color = '#37aee2';
+  </script>
+  
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={color} width={size} height={size}>
+    <path d="M12 8l-5 5h3v6h4v-6h3l-5-5z" />
+  </svg>
+  
