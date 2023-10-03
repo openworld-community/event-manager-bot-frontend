08ed2295c67f3b03d0858382e96f986a6f2b250a
hellokkki <akibigbabytape@gmail.com>
2023-10-03 16:33:36 +0300

    fix typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import Footer from "$lib/components/Footer.svelte";
     import Header from "$lib/components/Header.svelte";
     import { showPopup } from "$lib/logic/showPopup";
-    import MenuPopap from "$lib/components/ui/MenuPopap.svelte"; 
+    import MenuPopap from "$lib/components/ui/MenuPopup.svelte"; 
 </script>
 
 <Header />
